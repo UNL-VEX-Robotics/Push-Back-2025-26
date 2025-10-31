ef7ced189597e5b04cac226812397d7dd344a835
bryceclosman <bryceclosman@gmail.com>
2025-10-31 11:45:46 -0500

    Created Intake class

---
+++ include/intake.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "vex.h"
+#include "neblib/devices.hpp"
+
+class Intake
+{
+private:
+    vex::motor &topMotor;
+    vex::motor &middleMotor;
+    vex::motor_group frontMotors;
+    vex::motor_group mainMotors;
+
+    neblib::Cylinder &hood;
+    neblib::CylinderGroup &lift;
+    neblib::CylinderGroup &front;
+
+    vex::optical &colorSensor;
+
+    double velocity;
+    bool running;
+
+public:
+    Intake(vex::motor_group &&frontMotors, vex::motor_group &&mainMotors, vex::motor &topMotor, vex::motor &middleMotor, neblib::Cylinder &hood, neblib::CylinderGroup &lift, neblib::CylinderGroup &front, vex::optical &colorSensor);
+
+    void startLoop();
+    void stopLoop();
+
+    void setSpeed(double velocity);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include "vex.h"
 #include "neblib/xdrive.hpp"
+#include "intake.hpp"
 #include <iostream>
 
 using namespace vex;
@@ -19,40 +20,6 @@ competition Competition;
 // define your global instances of motors and other devices here
 brain Brain;
 controller controller1(primary);
-
-vex::motor LFT = vex::motor(PORT12, ratio6_1, false);
-vex::motor LFB = vex::motor(PORT13, ratio6_1, true);
-vex::motor RFT = vex::motor(PORT3, ratio6_1, true);
-vex::motor RFB = vex::motor(PORT2, ratio6_1, false);
-vex::motor LBT = vex::motor(PORT14, ratio6_1, false);
-vex::motor LBB = vex::motor(PORT15, ratio6_1, true);
-vex::motor RBT = vex::motor(PORT5, ratio6_1, true);
-vex::motor RBB = vex::motor(PORT4, ratio6_1, false);
-
-vex::rotation parallel(PORT11);
-vex::rotation perpendicular(PORT1);
-vex::inertial imu(PORT16);
-vex::distance dist(PORT17);
-
-neblib::MCL mcl(
-    { new neblib::Distance(dist, 0.0, 0.0, 0.0) },
-    std::unique_ptr<neblib::TrackerWheel>(new neblib::RotationTrackerWheel(parallel, 2.0)),
-    2.0,
-    std::unique_ptr<neblib::TrackerWheel>(new neblib::RotationTrackerWheel(perpendicular, 2.0)),
-    2.0,
-    imu,
-    100,
-    {
-        neblib::Line(neblib::Point(-72.0, -72.0), neblib::Point(72.0, -72.0)),
-        neblib::Line(neblib::Point(72.0, 72.0), neblib::Point(72.0, -72.0)),
-        neblib::Line(neblib::Point(-72.0, 72.0), neblib::Point(72.0, 72.0)),
-        neblib::Line(neblib::Point(-72.0, 72.0), neblib::Point(-72.0, -72.0))
-    },
-    1.0,
-    0.1
-);
-
-neblib::XDrive xDrive(vex::motor_group(LFT, LFB), vex::motor_group(RFT, RFB), vex::motor_group(LBT, LBB), vex::motor_group(RBT, RBB), &mcl, imu);
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -88,35 +55,9 @@ void autonomous(void) {
 
 
 void usercontrol(void) {
-
-  imu.calibrate();
-  task::sleep(2000);
-
-  mcl.setPose(-48.0, -24.0, 270);
-  int time = Brain.Timer.time();
  
   while (true)
   {
-    mcl.update();
-    neblib::Pose e = mcl.getPose();
-    Brain.Screen.clearScreen();
-    Brain.Screen.setCursor(1, 1);
-    Brain.Screen.print("x: ");
-    Brain.Screen.print(e.x);
-
-    Brain.Screen.setCursor(2, 1);
-    Brain.Screen.print("y: ");
-    Brain.Screen.print(e.y);
-
-    Brain.Screen.setCursor(3, 1);
-    Brain.Screen.print("h: ");
-    Brain.Screen.print(e.heading);
-
-    Brain.Screen.setCursor(4, 1);
-    Brain.Screen.print("t: ");
-    Brain.Screen.print(Brain.Timer.time() - time);
-    time = Brain.Timer.time();
-    
     task::sleep(10);
   }
 }
